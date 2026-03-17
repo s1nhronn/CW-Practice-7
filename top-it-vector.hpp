9e8e03950b931d212da269a02d9aa1f00321b42c
s1nhronn <silekatik@yandex.ru>
2026-03-17 13:26:09 +0300

    failed test

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -8,7 +8,9 @@ namespace topit
   struct Vector
   {
     Vector();
+    Vector(const Vector< T >&) = delete;
     ~Vector();
+    Vector< T >& operator=(const Vector< T >&) = delete;
 
     bool isEmpty() const noexcept;
 
@@ -16,6 +18,23 @@ namespace topit
     T* data_;
     size_t size_, capacity_;
   };
+}
+
+template < class T >
+bool topit::Vector< T >::isEmpty() const noexcept
+{
+  return false;
+}
+
+template < class T >
+topit::Vector< T >::Vector(): data_(nullptr), size_(0), capacity_(0)
+{
+}
+
+template < class T >
+topit::Vector< T >::~Vector()
+{
+  delete[] data_;
 }
 
 #endif
