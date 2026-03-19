1a3ce768a82a0a422537e6ff5702e7273cf76064
s1nhronn <silekatik@yandex.ru>
2026-03-19 15:01:32 +0300

    Ultra mega super puper duper Uber update

--- main.cpp
+++ main.cpp
@@ -14,12 +14,35 @@ bool testVectorWithValue()
   return !v.isEmpty();
 }
 
+bool testElementAccess()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  v.pushBack(2);
+  return v[0] == 1 && v[1] == 2;
+}
+
+bool testCopyConstructor()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  topit::Vector< int > yav = v;
+  bool isAllEqual = v.getSize() == yav.getSize();
+  for (size_t i = 0; isAllEqual && i < v.getSize(); ++i)
+  {
+    isAllEqual = isAllEqual && (v[i] == yav[i]);
+  }
+  return isAllEqual;
+}
+
 int main()
 {
   using test_t = bool (*)();
   using pair_t = std::pair< const char*, test_t >;
   pair_t tests[] = {{"Default vector should be empty", testDefaultVector},
-                    {"Vector with any value is not empty", testVectorWithValue}};
+                    {"Vector with any value is not empty", testVectorWithValue},
+                    {"Inbound access elements", testElementAccess},
+                    {"Sizes must be equal as elements", testCopyConstructor}};
   const size_t count = sizeof(tests) / sizeof(pair_t);
   std::cout << std::boolalpha;
   bool pass = true;
