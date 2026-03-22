a5c45348a7078f92effe613d552e2201eec72592
s1nhronn <silekatik@yandex.ru>
2026-03-22 20:58:50 +0300

    getCapacity compiled test

--- main.cpp
+++ main.cpp
@@ -62,6 +62,13 @@ bool testDoublePushFront()
   return v[0] == 2 && v[1] == 1;
 }
 
+bool testCorrectOfCapacity()
+{
+  topit::Vector< int > v;
+  v.pushBack(1);
+  return v.getCapacity() == 1;
+}
+
 int main()
 {
   using test_t = bool (*)();
