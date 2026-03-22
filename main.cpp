0113f352b50be0916314d58ec85df9a287a041cb
s1nhronn <silekatik@yandex.ru>
2026-03-22 20:56:40 +0300

    one more pushFront test successed

--- main.cpp
+++ main.cpp
@@ -54,6 +54,14 @@ bool testPushFront()
   return v[0] == 3 && v[1] == 1 && v[2] == 2;
 }
 
+bool testDoublePushFront()
+{
+  topit::Vector< int > v;
+  v.pushFront(1);
+  v.pushFront(2);
+  return v[0] == 2 && v[1] == 1;
+}
+
 int main()
 {
   using test_t = bool (*)();
@@ -63,7 +71,8 @@ int main()
                     {"Inbound access elements", testElementAccess},
                     {"Sizes must be equal as elements", testCopyConstructor},
                     {"The data is valid when swapping places", testSwap},
-                    {"The element should be added to the beginning", testPushFront}};
+                    {"The element should be added to the beginning", testPushFront},
+                    {"Correctly adding two elements to the beginning", testDoublePushFront}};
   const size_t count = sizeof(tests) / sizeof(pair_t);
   std::cout << std::boolalpha;
   bool pass = true;
