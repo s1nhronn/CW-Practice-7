62b6d35660e638595943b2fd8126889bf8f6f556
s1nhronn <silekatik@yandex.ru>
2026-03-27 15:43:50 +0300

    Compile tests for range insertion

--- main.cpp
+++ main.cpp
@@ -420,6 +420,180 @@ bool testEraseValueOutOfRange()
   }
 }
 
+bool testInsertRangeFromEmpty()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  topit::Vector< int > v2;
+  try
+  {
+    v1.insert(0, v2, 0, 0);
+    return false;
+  }
+  catch (const std::range_error&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeInEmpty()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  v1.pushBack(3);
+  topit::Vector< int > v2;
+  try
+  {
+    v2.insert(0, v1, 0, 2);
+    return v1[0] == 1 && v1[1] == 2;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeV1SizeLessV2Range()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  topit::Vector< int > v2;
+  v2.pushBack(3);
+  v2.pushBack(4);
+  v2.pushBack(5);
+  v2.pushBack(6);
+  try
+  {
+    v1.insert(1, v2, 1, 4);
+    bool res = v1[0] == 1;
+    res = res && v1[1] == 4;
+    res = res && v1[2] == 5;
+    res = res && v1[3] == 6;
+    res = res && v1[4] == 2;
+    return res;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeV1SizeMoreV2Range()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  v1.pushBack(3);
+  v1.pushBack(4);
+  topit::Vector< int > v2;
+  v2.pushBack(5);
+  v2.pushBack(6);
+  v2.pushBack(7);
+  v2.pushBack(8);
+  try
+  {
+    v1.insert(2, v2, 1, 3);
+    bool res = v1[0] == 1;
+    res = res && v1[1] == 2;
+    res = res && v1[2] == 5;
+    res = res && v1[3] == 6;
+    res = res && v1[4] == 3;
+    res = res && v1[5] == 4;
+    return res;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeIndexOutOfRange()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  v1.pushBack(3);
+  v1.pushBack(4);
+  topit::Vector< int > v2;
+  v2.pushBack(5);
+  v2.pushBack(6);
+  v2.pushBack(7);
+  v2.pushBack(8);
+  try
+  {
+    v1.insert(10, v2, 1, 3);
+    return false;
+  }
+  catch (const std::out_of_range&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeBeginMoreThanEnd()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  v1.pushBack(3);
+  v1.pushBack(4);
+  topit::Vector< int > v2;
+  v2.pushBack(5);
+  v2.pushBack(6);
+  v2.pushBack(7);
+  v2.pushBack(8);
+  try
+  {
+    v1.insert(2, v2, 2, 1);
+    return false;
+  }
+  catch (const std::range_error&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool testInsertRangeEndOutOfRange()
+{
+  topit::Vector< int > v1;
+  v1.pushBack(1);
+  v1.pushBack(2);
+  v1.pushBack(3);
+  v1.pushBack(4);
+  topit::Vector< int > v2;
+  v2.pushBack(5);
+  v2.pushBack(6);
+  v2.pushBack(7);
+  v2.pushBack(8);
+  try
+  {
+    v1.insert(2, v2, 1, 10);
+    return false;
+  }
+  catch (const std::range_error&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
 int main()
 {
   using test_t = bool (*)();
@@ -455,7 +629,14 @@ int main()
       {"Erase from empty vector throws an exception", testEraseValueFromEmptyVector},
       {"Erase from vector with one value make it empty", testEraseValueFromVectorWithOneValue},
       {"Erase from empty vector with values its size decreases", testEraseValueFromVectorWithManyValues},
-      {"Erase from out of range throw an exception", testEraseValueOutOfRange}};
+      {"Erase from out of range throw an exception", testEraseValueOutOfRange},
+      {"Insert from empty throw an exception", testInsertRangeFromEmpty},
+      {"Insert in empty push back elems", testInsertRangeInEmpty},
+      {"Insert in vector with size less than paste range from v2", testInsertRangeV1SizeLessV2Range},
+      {"Insert in vector with size more than paste range from v2", testInsertRangeV1SizeMoreV2Range},
+      {"Insert range in vector when index more than size throw an exception", testInsertRangeIndexOutOfRange},
+      {"Insert range in vector when begin more than end throw an exception", testInsertRangeBeginMoreThanEnd},
+      {"Insert range in vector when end more than size throw an exception", testInsertRangeEndOutOfRange}};
   const size_t count = sizeof(tests) / sizeof(pair_t);
   std::cout << std::boolalpha;
   bool pass = true;
